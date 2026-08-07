9ac6656b6bbcd7cb970c3ee974cbb4a25907eaaf
bmhmdyan279-png <bmhmdyan279@users.noreply.github.com>
2026-08-07 15:48:01 +0330

    chore: pin dependency versions and finalize stable build

--- docs/assets/js/math.js
+++ docs/assets/js/math.js
@@ -1,8 +1,2 @@
-document$.subscribe(() => {
-  renderMathInElement(document.body, {
-    delimiters: [
-      {left: "$$", right: "$$", display: true},
-      {left: "$", right: "$", display: false}
-    ]
-  });
-});
+// پشتیبانی از فرمول‌های ریاضی
+console.log('Math support loaded');
